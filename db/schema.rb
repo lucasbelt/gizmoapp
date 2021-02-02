e63aac08e218c7c57e2856f39071cd92e93e5da2
Luis Carlos Correa <luiscarloscorreamartinez@gmail.com>
2021-02-02 11:29:12 -0500

    Add chef and recipe model validatios with tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_233438) do
+ActiveRecord::Schema.define(version: 2021_02_01_141137) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string "chefname"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
---
+++ test/models/chef_test.rb
@@ -0,0 +1,63 @@
+ require "test_helper"
+
+ class ChetTest < ActiveSupport::TestCase
+
+  def setup
+    @chef = Chef.new(chefname: "lucasbelt", email: "lucasbelt@hotmail.com")
+  end
+
+  test "should be valid" do
+    assert @chef.valid?
+  end
+
+  test "Name should be present" do
+    @chef.chefname = " "
+    assert_not @chef.valid?
+  end
+
+  test "Name should be less than 30 characters" do
+    @chef.chefname = "a" * 31
+    assert_not @chef.valid?
+  end
+
+  test "Email should be present" do
+    @chef.email = " "
+    assert_not @chef.valid?
+  end
+
+  test "Email should not too long" do
+    @chef.email = "a" * 245 + "@example.com"
+    assert_not @chef.valid?
+  end
+
+  test "Email should accept correct format" do
+    valid_emails = %w[user@example.com lucasbelt@hotmail.com l.first@yahoo.ca john+smith@co.uk.org]
+    valid_emails.each do |valids|
+      @chef.email = valids
+      assert @chef.valid?, "#{valids.inspect} should be valid"
+    end
+  end
+
+  test "Should reject invalid addresses" do
+    invalid_emails = %w[user@example lucasbelt@hotmail,com l.first@yahoo. john@bar+uk.org]
+    invalid_emails.each do |invalids|
+      @chef.email = invalids
+      assert_not @chef.valid?, "#{invalids.inspect} should be invalid"
+    end
+  end
+
+  test "Email should be unique and case insensitive" do
+    duplicate_chef = @chef.dup
+    duplicate_chef.email = @chef.email.upcase
+    @chef.save
+    assert_not duplicate_chef.valid?
+  end
+
+  test "Email should be lower case before hitting db" do
+    mixed_email = "John@Example.com"
+    @chef.email = mixed_email
+    @chef.save
+    assert_equal mixed_email.downcase, @chef.reload.email
+  end
+
+ end
